832a1477fc6d050a439497d998c649bbb2b18769
soumyakarwa <soumyakarwa01@gmail.com>
2025-04-03 22:14:36 -0400

    adding frontend'

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/endpoint", (req, res) => {
+  res.json({ message: "Hello from the backend!" });
 });
 
 app.listen(port, () => {
